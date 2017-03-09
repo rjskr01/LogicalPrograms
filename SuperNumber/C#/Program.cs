e5a785800c7c455cf4d3f57b61e64055d5fbda46
sathishbaburathinavel <sathishbabur@hotmail.com>
2017-03-09 23:55:31 +0530

    Added the C# and scala source

---
+++ SuperNumber/C#/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SuperNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            watch.Start();
+            var superNumbers = new int[9];
+            for (int j = 2; j <= 9; j++)
+            {
+                superNumbers = new int[j];
+                var possibilityCount = (int)Math.Pow(j, j);
+                for (int i = 0; i < possibilityCount - 1; i++)
+                {
+                    incrementArray(superNumbers, superNumbers.Length - 1);
+                    if (isSuperNumber(superNumbers))
+                    {
+                        foreach (int digit in superNumbers)
+                            Console.Write(digit);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine(watch.Elapsed);
+            Console.ReadLine();
+        }
+        public static void incrementArray(int[] possibilities, int digitIndex)
+        {
+            var tmp = 0;
+            var digitCount = possibilities.Length;
+            tmp = possibilities[digitIndex] + 1;
+            if (tmp % digitCount == 0)
+            {
+                possibilities[digitIndex] = 0;
+                incrementArray(possibilities, digitIndex - 1);
+            }
+            else
+                possibilities[digitIndex] = tmp;
+        }
+        public static bool isSuperNumber(int[] superNumbers)
+        {
+            var count = 0;
+            var digitCount = superNumbers.Length;
+            for (int index = 0; index < digitCount; index++)
+            {
+                count = 0;
+                foreach (int digit in superNumbers)
+                {
+                    if (digit == index)
+                        count++;
+                }
+                if (count != superNumbers[index])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SuperNumber/Scala/SuperNumber.scala
@@ -0,0 +1,57 @@
+
+object SuperNumber
+{
+	def main(args: Array[String])
+	{
+		val start=System.nanoTime()
+		
+		for(j<-2 to 9)
+		{
+			var superNumbers=new Array[Int](j)
+			val possibilityCount : Int=math.pow(j,j).toInt
+			for(i<-0 until possibilityCount-1)
+			{
+				incrementArray(superNumbers,superNumbers.length-1)
+				if(isSuperNumber(superNumbers))
+				{
+					superNumbers.foreach(print)
+					println
+				}
+			}
+		}
+		val endTime :Double=System.nanoTime().toDouble
+		val seconds :Double = (endTime-start) / 1000000000.0d
+
+		println("Total Seconds:"+seconds)
+	}
+
+	def incrementArray(possibilities: Array[Int],digitIndex:Int)
+	{
+		var tmp:Int=0
+		var digitCount:Int = possibilities.length
+		tmp=possibilities(digitIndex)+1
+			if(tmp%digitCount==0)
+			{
+				possibilities(digitIndex)=0
+				incrementArray(possibilities,digitIndex-1)
+			}
+			else
+				possibilities(digitIndex)=tmp
+	}
+	def isSuperNumber(superNumbers :Array[Int]) : Boolean  =	{
+		var count:Int=0
+		val digitCount=superNumbers.length
+		(0 until digitCount).foreach((index)=>{
+			count=0
+			superNumbers.foreach((digit)=>{
+				if (digit == index)
+					count=count+1
+			})
+			
+			if(count!=superNumbers(index))
+				return false
+		})
+		return true
+	}
+	
+}
